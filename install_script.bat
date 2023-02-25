4f9249415dc7f6a8080cb040a883b76b675b3627
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-02-25 00:19:13 -0300

    Update install_script.bat

--- install_script.bat
+++ install_script.bat
@@ -2,10 +2,7 @@
 title Installing software with Chocolatey
 
 REM Install Chocolatey if it's not already installed
-if not exist %systemdrive%\chocolatey (
-    echo Installing Chocolatey...
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 REM Install packages with Chocolatey
 echo Installing packages...
