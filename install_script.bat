3bed09a52b3a2aea585234e06308381f7cdaea52
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-02-25 00:16:57 -0300

    Update install_script.bat

--- install_script.bat
+++ install_script.bat
@@ -1,30 +1,40 @@
-@echo of
+@echo off
+title Installing software with Chocolatey
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+REM Install Chocolatey if it's not already installed
+if not exist %systemdrive%\chocolatey (
+    echo Installing Chocolatey...
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+)
 
-choco install --confirm adobereader
-choco install --confirm googlechrome
-choco install --confirm flashplayerplugin
-choco install --confirm jre8
-choco install --confirm 7zip.install
-choco install --confirm microsoft-windows-terminal
-choco install --confirm onlyoffice
-choco install --confirm curl
-choco install --confirm spotify
-choco install --confirm amd-ryzen-chipset
-choco install --confirm nvidia-display-driver
-choco install --confirm qbittorrent
-choco install --confirm openvpn
-choco install --confirm steam
-choco install --confirm discord
-choco install --confirm bitwarden
-choco install --confirm directx
-choco install --confirm msvisualcplusplus2013-redist
-choco install --confirm origin
-choco install --confirm epicgameslauncher
-choco install --confirm leagueoflegends
+REM Install packages with Chocolatey
+echo Installing packages...
+for %%p in (
+    adobereader
+    googlechrome
+    flashplayerplugin
+    jre8
+    7zip.install
+    microsoft-windows-terminal
+    onlyoffice
+    curl
+    spotify
+    amd-ryzen-chipset
+    nvidia-display-driver
+    qbittorrent
+    openvpn
+    steam
+    discord
+    bitwarden
+    directx
+    msvisualcplusplus2013-redist
+    origin
+    epicgameslauncher
+    leagueoflegends
+    teamspeak
+    choco-cleaner
+) do choco install --confirm %%p
 
-choco install --confirm teamspeak
-
-choco install --confirm choco-cleaner
 Choco-Cleaner
+
+echo Installation completed.
