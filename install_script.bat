9f454749fad563bb11111c9fced5cb80dc116963
Egydio Bolonhezi <egydiobolonhezi@gmail.com>
2023-02-25 00:25:26 -0300

    Update install_script.bat

--- install_script.bat
+++ install_script.bat
@@ -1,7 +1,5 @@
 @echo of
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
 choco install --confirm adobereader
 choco install --confirm googlechrome
 choco install --confirm flashplayerplugin
